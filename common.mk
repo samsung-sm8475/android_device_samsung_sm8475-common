e6497a2be0acb3892c21cf201d648833358bf4a9
David Wronek <david@mainlining.org>
2025-05-04 08:27:10 +0200

    sm8650-common: Import QCOM common configs
    Co-authored-by: chaptsand <chaptsand@gmail.com>
    Signed-off-by: David Wronek <david@mainlining.org>
    Change-Id: I50a49ad81e87d81fec768028968ff62f10cfd774

--- common.mk
+++ common.mk
@@ -41,6 +41,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
